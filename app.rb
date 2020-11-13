14b39a746b1fdcbc1379826de79f399e2db2b47b
Joshua Parco <jacparco@gmail.com>
2020-11-12 17:09:43 -0700

    deleted test file

--- app.rb
+++ app.rb
@@ -1,23 +1,27 @@
 require_relative "menu.rb"
 main_dishes = {
   message: "Which main dish would you like to purchase?", 
-  options: ["Chicken", "Beef", "Pork"]
+  options: [{"Chicken": "$5.00"}, {"Beef": "$4.50"}, {"Pork": "$5.50"}]
 }
 side_dishes = {
-  message: "Which side dish would you like to purchase?"
-  options: ["Fries", "Rice", "Vegetables"]
+  message: "Which side dish would you like to purchase?",
+  options: [{"Fries": "$1.50"}, {"Rice": "$1.00"}, {"Vegetables": "$2.00"}]
 }
 
-main_menu_test = Menu.new(main_dishes)
-side_dishes_test = Menu.new(side_dishes)
+user_chooses_main_dish = Menu.new(main_dishes)
+user_chooses_side_dish = Menu.new(side_dishes)
 
-main_menu_test.print
-selection1 = main_menu_test.get_selection
+user_chooses_main_dish.print
+selection1 = user_chooses_main_dish.get_selection
 puts selection1
 
-main_menu_test.print
-selection1 = main_menu_test.get_selection
-puts selection1
+user_chooses_side_dish.print
+selection2 = user_chooses_side_dish.get_selection
+puts selection2
+
+user_chooses_side_dish.print
+selection3 = user_chooses_side_dish.get_selection
+puts selection3
 # ask user for a choice
 # the user chooses 2 side dish items
 # computer repeats users orders
